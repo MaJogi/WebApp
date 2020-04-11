605e1dc6513d1b736e271983111be8449fdb3ce6
MaJogi <47116426+MaJogi@users.noreply.github.com>
2020-04-11 11:23:39 +0300

    Esialgne commit. Lisasin kihilise arhitektuuri.

--- userSupportWebApp/Startup.cs
+++ userSupportWebApp/Startup.cs
@@ -1,19 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using userSupportWebApp.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using userSupportWebApp.Data;
+using WebApp.userSupportWebApp.Data;
 
-namespace userSupportWebApp
+namespace WebApp.userSupportWebApp
 {
     public class Startup
     {
